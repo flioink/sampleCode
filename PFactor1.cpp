52f2866799712efcd458f53080d2baa56e93eb4a
flioink <phantom_warrior@abv.bg>
2019-08-30 04:31:37 +0300

    New Excercise

---
+++ PFactor1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void prime(int n)
+{
+    int z = 2;
+
+    vector<int> st;
+
+    while(z * z <= n)
+    {
+        if(n% z == 0)
+        {
+            cout << "i: " << z << endl;
+            n /= z;
+            st.push_back(z);
+
+        }
+
+        else
+        {
+            z++;
+        }
+
+    }
+
+    if(n > 1)
+    {
+        cout << "Prime: " << n << endl;
+        st.push_back(n);
+    }
+
+    for(auto& it: st)
+    {
+        cout << it << endl;
+    }
+}
+
+
+int main()
+{
+    prime(900);
+
+
+    return 0;
+}
+
